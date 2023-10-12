55dd1305a9be61cb1e7139714fb1aaae06ae8464
Sandesh Shrestha <sandeshstha078@gmail.com>
2023-10-12 16:39:01 +0545

    added switch case

--- food_admin/lib/screens/home_screen.dart
+++ food_admin/lib/screens/home_screen.dart
@@ -1,97 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+import 'package:food_admin/screens/features/add_product.dart';
+import 'package:food_admin/screens/features/categorie.dart';
+import 'package:food_admin/screens/features/dashboard.dart';
+import 'package:food_admin/screens/features/delete_product.dart';
+import 'package:food_admin/screens/features/update_product.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  static const String id = 'home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Widget selectedScreen = const DashboardScreen();
+  chooseScreens(item) {
+    switch (item.route) {
+      case DashboardScreen.id:
+        setState(() {
+          selectedScreen = const DashboardScreen();
+        });
+        break;
+      case CategorieScreen.id:
+        setState(() {
+          selectedScreen = const CategorieScreen();
+        });
+        break;
+      case AddProductScreen.id:
+        setState(() {
+          selectedScreen = const AddProductScreen();
+        });
+        break;
+      case UpdateProductScreen.id:
+        setState(() {
+          selectedScreen = const UpdateProductScreen();
+        });
+        break;
+      case DeleteProductScreen.id:
+        setState(() {
+          selectedScreen = const DeleteProductScreen();
+        });
+        break;
+
+      default:
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return AdminScaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Sample'),
-      ),
-      sideBar: SideBar(
-        items: const [
-          AdminMenuItem(
-            title: 'Dashboard',
-            route: '/',
-            icon: Icons.dashboard,
-          ),
-          AdminMenuItem(
-            title: 'Top Level',
-            icon: Icons.file_copy,
-            children: [
-              AdminMenuItem(
-                title: 'Second Level Item 1',
-                route: '/secondLevelItem1',
-              ),
-              AdminMenuItem(
-                title: 'Second Level Item 2',
-                route: '/secondLevelItem2',
-              ),
-              AdminMenuItem(
-                title: 'Third Level',
-                children: [
-                  AdminMenuItem(
-                    title: 'Third Level Item 1',
-                    route: '/thirdLevelItem1',
-                  ),
-                  AdminMenuItem(
-                    title: 'Third Level Item 2',
-                    route: '/thirdLevelItem2',
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-        selectedRoute: '/',
-        onSelected: (item) {
-          if (item.route != null) {
-            Navigator.of(context).pushNamed(item.route!);
-          }
-        },
-        header: Container(
-          height: 50,
-          width: double.infinity,
-          color: const Color(0xff444444),
-          child: const Center(
-            child: Text(
-              'header',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Admin Panel',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
         ),
-        footer: Container(
-          height: 50,
-          width: double.infinity,
-          color: const Color(0xff444444),
-          child: const Center(
-            child: Text(
-              'footer',
-              style: TextStyle(
-                color: Colors.white,
-              ),
+        sideBar: SideBar(
+          textStyle: const TextStyle(
+              color: Color.fromARGB(146, 0, 0, 0), fontSize: 16),
+          items: const [
+            AdminMenuItem(
+              title: 'DASHBOARD',
+              route: DashboardScreen.id,
+              icon: Icons.dashboard,
             ),
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.all(10),
-          child: const Text(
-            'Dashboard',
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 36,
+            AdminMenuItem(
+              title: 'CATEGORIES',
+              route: CategorieScreen.id,
+              icon: Icons.category,
             ),
-          ),
+            AdminMenuItem(
+              title: 'ADD PRODUCT',
+              route: AddProductScreen.id,
+              icon: Icons.add,
+            ),
+            AdminMenuItem(
+              title: 'UPDATE PRODUCT',
+              route: UpdateProductScreen.id,
+              icon: Icons.update,
+            ),
+            AdminMenuItem(
+              title: 'DELETE PRODUCT',
+              route: DeleteProductScreen.id,
+              icon: Icons.delete,
+            ),
+          ],
+          selectedRoute: HomeScreen.id,
+          onSelected: (item) {
+            chooseScreens(item);
+          },
         ),
-      ),
-    );
+        body: selectedScreen);
   }
 }
