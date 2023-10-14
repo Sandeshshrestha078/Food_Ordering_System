13e2c853a7f2f5fa13d63aff017ba5d5a0debb3b
Sandesh Shrestha <sandeshstha078@gmail.com>
2023-10-14 16:08:12 +0545

    add constant file

---
+++ food_admin/lib/constant/values.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//Style
+const headerStyle = TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
+const titleStyle = TextStyle(
+  color: Color.fromRGBO(255, 255, 255, 1),
+  fontSize: 16,
+  fontWeight: FontWeight.w200,
+);
+const titleStyle2 = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+const chooseimg = TextStyle(fontSize: 14, color: Colors.blue);
+
+//Decoration
+const categorieStyle = BoxDecoration(
+    borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+    color: Color.fromRGBO(26, 112, 223, 1));
