a2b66be2a64058d4dd2341c0bdbefddc67b4e59f
Sandesh Shrestha <sandeshstha078@gmail.com>
2023-11-23 09:07:36 +0545

    food user added

---
+++ food_users/lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreeen extends StatelessWidget {
+  const HomeScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('home')),
+    );
+  }
+}
